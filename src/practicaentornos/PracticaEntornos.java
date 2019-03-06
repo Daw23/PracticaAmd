89e025fd913efa3be33c4c046aa47c3169809761
albertomd <albertomd@userDJ>
2019-03-06 16:41:55 +0100

    primer commit en fix

--- src/practicaentornos/PracticaEntornos.java
+++ src/practicaentornos/PracticaEntornos.java
@@ -12,6 +12,10 @@ public class PracticaEntornos {
   public static void main(String[] args) {
     System.out.println("Practica entornos ");
     
+    for (int i = 0; i < 10; i++) {
+      System.out.println(i);
+    }
+    
   }
   
 }
