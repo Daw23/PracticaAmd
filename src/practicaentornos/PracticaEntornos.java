e86b270c2808be55774ecffad0b8b4c62641b83d
albertomd <albertomd@userDJ>
2019-03-06 16:35:00 +0100

    primer commit en local

---
+++ src/practicaentornos/PracticaEntornos.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practicaentornos;
+
+
+public class PracticaEntornos {
+
+  
+  public static void main(String[] args) {
+    System.out.println("Practica entornos ");
+    
+  }
+  
+}
